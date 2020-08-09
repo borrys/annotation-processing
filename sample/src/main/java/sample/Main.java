81f7e96f7b34d7ac5a34ab9f3b1eb2c613c1b558
BorryS <andrzej.kimel@gmail.com>
2020-08-09 13:11:25 +0200

    multiple annotated enums

--- sample/src/main/java/sample/Main.java
+++ sample/src/main/java/sample/Main.java
@@ -1,7 +1,13 @@
 package sample;
 
+import java.util.stream.Stream;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println(ResourceTypeCompanion.toString(ResourceType.PORTABLE_NETWORK_GRAPHICS));
+    System.out.println("** Resource types **");
+    Stream.of(ResourceType.values()).map(rt -> ResourceTypeCompanion.toString(rt)).forEach(System.out::println);
+
+    System.out.println("** Source types **");
+    Stream.of(SourceType.values()).map(rt -> SourceTypeCompanion.toString(rt)).forEach(System.out::println);
   }
 }
---
+++ sample/src/main/java/sample/SourceType.java
@@ -0,0 +1,9 @@
+package sample;
+
+import annotationprocessing.ToStringCompanion;
+
+@ToStringCompanion
+public enum SourceType {
+  LOCAL_FILE,
+  NETWORK_RESOURCE
+}
